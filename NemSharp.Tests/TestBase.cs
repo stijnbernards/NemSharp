a20c62aeb04d2954a1ea989900975bb1363fdbb6
Stijn Bernards <Admin@heroicgamers.com>
2017-07-23 02:32:16 +0200

    Remove docker add testnet

--- NemSharp.Tests/TestBase.cs
+++ NemSharp.Tests/TestBase.cs
@@ -10,7 +10,7 @@ namespace NemSharp.Tests
         [SetUp]
         public void Initialize()
         {
-            Client = new NemClient("http://127.0.0.1:7890");
+            Client = new NemClient("http://bob.nem.ninja:7890");
         }
     }
 }
